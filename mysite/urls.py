5bb688b606b1bd17e61716578bf93c3c0fdf0a26
Your Name <kazymovlatif@gmaile.com>
2019-02-24 23:52:40 +0200

    Changed the HTML for the site.

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import re_path
+from django.urls import path, include
 
 urlpatterns = [
+    path('', include('blog.urls')),
     re_path('admin/', admin.site.urls),
 ]
